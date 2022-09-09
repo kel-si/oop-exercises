cda7bd2c00bf2a0eeef2fabb7dce8c9f501c7c6e
Kelsi Olstad <kelsiolstad@gmail.com>
2022-09-09 14:15:57 -0600

    class method

---
+++ 01-object-model/class_method.rb
@@ -0,0 +1,12 @@
+# Objects are created from classes. Anything in Ruby that is said to have a value is an object (ex: numbers, strings, arrays, classes, modules)
+
+# Methods, blocks, and variables are not objects
+
+# prints 'String'
+puts "hello".class
+
+# prints 'Integer'
+puts 7.class
+
+# prints 'Array'
+puts ["A", "B", 1].class
