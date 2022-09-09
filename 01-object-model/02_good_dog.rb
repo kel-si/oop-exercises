245c69e22f9b8821dad929b3012819ac15ed76d2
Kelsi Olstad <kelsiolstad@gmail.com>
2022-09-09 14:23:21 -0600

    define a class and create a new instance

---
+++ 01-object-model/02_good_dog.rb
@@ -0,0 +1,9 @@
+# CamelCase naming convention for classes
+# File names should be in snake_case and reflect the class name
+class GoodDog
+end
+
+# An instance of our GoodDog class is stored in a variable called frosty
+
+# An object is returned by calling the class method 'new'
+frosty = GoodDog.new
