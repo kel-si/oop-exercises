9b0483bd62998f02f34c9942996ad592161f0767
Kelsi Olstad <kelsiolstad@gmail.com>
2022-09-10 17:03:59 -0600

    getters and setters to get and update an instance variable

---
+++ 02-classes-objects-1/05_getters_and_setters.rb
@@ -0,0 +1,29 @@
+class GoodDog
+  def initialize name
+    @name = name
+  end
+
+  # Getter method
+  def name
+    @name
+  end
+
+  # Setter method to set name after initializing
+  def name= n
+    @name = n
+    # Setter methods always return the value passed in as an argument
+    "Frosters" # <= will get ignored
+  end
+
+  def speak
+    "#{@name} says ruff!"
+  end
+end
+
+frosty = GoodDog.new "Frosty"
+
+puts frosty.speak
+puts frosty.name # => "Frosty"
+
+frosty.name = 'Frosty Frosterton'
+puts frosty.name # => "Frosty Frosterton"
